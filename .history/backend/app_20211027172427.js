d4d2924e5ac2c2dd3dc88e423fd0d05de09d0a7e
akmalhakimteo <akmal_hakim_teo@hotmail.com>
2021-10-27 11:52:20 +0800

    limit node version due to bug. See @https://github.com/webpack/webpack/issues/14532

---
+++ .history/backend/app_20211027172427.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const utils = require("./utils/errors");
+const db = require("./models");
+const config = require("./config/db.config")
+app.use(cors());
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+// config.sequelize.authenticate()
+config.sequelize.sync();
+config.sequelize.authenticate()
+.then(()=>{
+  console.log("Successfully connected to MYSQL DB")
+})
+.catch((err)=>{
+  console.error("Error connecting to MYSQL DB",err)
+  process.exit(1)
+})
+
+app.use(express.static("build"));
+app.use("/", require("./controllers/index"));
+app.use("/api/url", require("./controllers/url"));
+app.use(utils.malformedURL);
+app.use(utils.unknownEndpoint);
+
+module.exports = app;
